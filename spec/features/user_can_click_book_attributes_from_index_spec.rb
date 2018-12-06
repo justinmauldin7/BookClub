39c1dbbd0aa65ca866ff95021ebf6a155c34d7a9
jpclark6 <jpclark6@gmail.com>
2018-12-06 14:10:38 -0700

    Adds link to author page from index

--- spec/features/user_can_click_book_attributes_from_index_spec.rb
+++ spec/features/user_can_click_book_attributes_from_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 describe 'on the index page' do
   describe 'as a user' do
-    it 'can click a title to go to the book show page' do
+    before(:each) do
       @book_1 = Book.create(title:'How To Test Ruby', pages: 2, year: 1934)
       @book_2 = Book.create(title:'How To Test Diamonds', pages: 3853, year: 1895)
 
@@ -23,13 +23,24 @@ describe 'on the index page' do
       @review_4 = Review.create(title: 'This Book Was Horrible', rating: 1, description: 'Bad Bad Bad', user_id: @user_2.id, book_id: @book_2.id)
       @review_5 = Review.create(title: 'This Book Was Terrific', rating: 4, description: 'Super x99', user_id: @user_3.id, book_id: @book_2.id)
       @review_6 = Review.create(title: 'This Book Was Abyssmal', rating: 2, description: 'Worse than bad', user_id: @user_4.id, book_id: @book_2.id)
+    end
 
+    it 'can click a title to go to the book show page' do
       visit books_path
       click_on @book_1.title
 
       expect(current_path).to eq(book_path(@book_1.id))
       expect(page).to have_content(@book_1.title)
-      expect(page).to have_no_content(@book_2.title)      
+      expect(page).to have_no_content(@book_2.title)
+    end
+
+    it 'can click an author to go to the author show page' do
+      visit books_path
+      click_on @book_1.authors.first.name
+
+      expect(current_path).to eq(author_path(@book_1.authors.first.id))
+      expect(page).to have_content(@book_1.authors.first.name)
+      expect(page).to have_no_content(@book_2.authors.first.name)
     end
   end
 end
