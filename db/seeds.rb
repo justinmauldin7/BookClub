9f3365e2603b23c96685c5bd8ec4e9d258d03abe
jpclark6 <jpclark6@gmail.com>
2018-12-11 15:18:07 -0700

    Adds styling and seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,24 +7,39 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'faker'
-require 'pry'
-# books, authors, users, reviews
 20.times do
   user = Faker::HarryPotter.character
   User.create(username: user)
 end
 
-40.times do
+10.times do
   name = Faker::Book.author
   title = Faker::Book.title
   pages = rand(50..2000)
   year = rand(1843..2018)
   author = Author.create(name: name)
   book = Book.create!(title: title, pages: pages, year: year, authors: [author])
-  4.times do
+  (rand(1..8)).times do
     review_title = Faker::StarWars.quote
     rating = rand(1..5)
     description = Faker::MichaelScott.quote
+    Review.create(title: review_title, rating: rating, description: description, user: User.find(rand(User.first.id..User.last.id)), book_id: book.id)
+  end
+end
+
+10.times do
+  name = Faker::FunnyName.three_word_name
+  name2 = Faker::FunnyName.three_word_name
+  title = Faker::LordOfTheRings.location
+  pages = rand(50..2000)
+  year = rand(1843..2018)
+  author = Author.create(name: name)
+  author2 = Author.create(name: name2)
+  book = Book.create!(title: title, pages: pages, year: year, authors: [author, author2])
+  (rand(1..8)).times do
+    review_title = Faker::StarTrek.specie
+    rating = rand(1..5)
+    description = Faker::MostInterestingManInTheWorld.quote
     Review.create(title: review_title, rating: rating, description: description, user: User.find(rand(User.first.id..User.last.id)), book_id: book.id)
   end
 end
