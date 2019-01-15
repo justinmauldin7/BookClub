968553b16293d592e11ff2dd2410f3b8deff380d
Justin Mauldin <justin.mauldin7@gmail.com>
2019-01-15 12:49:09 -0700

    Edits routes for mod 3 prework

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'books#home'
 
-  resources :books, except: [:update] do
-    resources :reviews, only: [:new, :create, :destroy]
-  end
+  get '/', to: 'books#home', as: 'root'
 
-  resources :authors, only: [:show, :destroy]
+  get '/books', to: 'books#index', as: 'books'
+  post '/books', to: 'books#create'
+  get '/books/new', to: 'books#new', as: 'new_book'
+  get '/books/:id', to: 'books#show', as: 'book'
+  delete '/books/:id', to: 'books#destroy'
 
-  resources :users, only: [:show]
+  get '/books/:book_id/reviews/new', to: 'reviews#new', as: 'new_book_review'
+  post '/books/:book_id/reviews', to: 'reviews#create', as: 'book_reviews'
+  delete '/books/:book_id/reviews/:id', to: 'reviews#destroy', as: 'book_review'
+
+  get '/authors/:id', to: 'authors#show', as: 'author'
+  delete '/authors/:id', to: 'authors#destroy'
+
+  get '/users/:id', to: 'users#show', as: 'user'
 end
